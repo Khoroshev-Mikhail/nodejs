8e3d5227ba47324aac1147e00fccc3bf69b66cd7
Mikha38 <oyks1379@yandex.ru>
2022-08-01 21:20:05 +0300

    home

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
-const Emitter = require('events')
-let emitter = new Emitter();
-let eventName = 'greeet'
-emitter.on(eventName, ()=> {
-    console.log('hello all')
-})
-emitter.on(eventName, ()=> {
-    console.log('привет')
-})
-emitter.emit('greeet')
+const fs = require('fs')
+const zlib = require('zlib')
+
+let readbleStream = fs.createReadStream('Hello.txt', 'utf-8')
+
+let writebleStream = fs.createWriteStream('Hello.txt.gz')
+
+let gzip = zlib.createGzip()
+
+readbleStream.pipe(gzip).pipe(writebleStream)
