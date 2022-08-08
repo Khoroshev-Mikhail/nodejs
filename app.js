3f310ebd434267c41d679fed5df018f6028600cf
Mikha38 <oyks1379@yandex.ru>
2022-08-08 14:15:55 +0300

    added set endpoints

--- app.js
+++ app.js
@@ -33,12 +33,45 @@ app.post('/setVocabulary', jsonParser, (req, res) => {
         return res.sendStatus(400)
     }
     const filePath = __dirname + '/baseData/vocabulary.json'
+    const data = fs.readFileSync(filePath, 'utf-8')
     const method = req.body.method
     const idWord = req.body.idWord
-    const data = fs.readFileSync(filePath, 'utf-8')
     let vocabulary = JSON.parse(data)
     vocabulary = {...vocabulary, [method] : [...vocabulary[method], idWord]}
     fs.writeFileSync(filePath, JSON.stringify(vocabulary))
+    res.sendStatus(200)
+})
+
+app.post('/setGroups', jsonParser, (req, res) => {
+    if(!req.body){
+        return res.sendStatus(400)
+    }
+    const filePath = __dirname + '/baseData/groups.json'
+    const data = fs.readFileSync(filePath, 'utf-8')
+    const eng = req.body.eng
+    const title = req.body.title
+    let groups = JSON.parse(data)
+    //groups = [...groups, {eng, title}] после проверки перерефакторить на это, просто не уверен что так сработает!
+    groups = [...groups, {eng: eng, title: title}]
+    fs.writeFileSync(filePath, JSON.stringify(groups))
+    res.sendStatus(200)
+})
+
+app.post('/setDictionary', jsonParser, (req, res) => {
+    if(!req.body){
+        return res.sendStatus(400)
+    }
+    const filePath = __dirname + '/baseData/dictionary.json'
+    const data = fs.readFileSync(filePath, 'utf-8')
+    let dictionary = JSON.parse(data)
+    const eng = req.body.eng
+    const rus = req.body.rus
+    const groups = req.body.groups
+    const id = Math.max(...dictionary.map(el => el.id)) + 1
+    //const newWord = {id: id, eng: eng, rus: rus, groups: groups}
+    const newWord = {id, eng, rus, groups}
+    dictionary = [...dictionary, newWord]
+    fs.writeFileSync(filePath, JSON.stringify(dictionary))
     res.sendStatus(200)
 })
 
